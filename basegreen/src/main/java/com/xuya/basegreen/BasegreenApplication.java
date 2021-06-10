bddf880c35919972ea99deb4ce017eaac06cb914
高栩亚 <gaoxuya@koukus-MacBook-Air.local>
2021-06-09 21:04:10 -0400

    initial commit

---
+++ basegreen/src/main/java/com/xuya/basegreen/BasegreenApplication.java
@@ -0,0 +1,13 @@
+package com.xuya.basegreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasegreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasegreenApplication.class, args);
+	}
+
+}
---
+++ basegreen/src/test/java/com/xuya/basegreen/BasegreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuya.basegreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasegreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
